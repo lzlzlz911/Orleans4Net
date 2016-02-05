d0a0486627affbaa9b71edb2ac49381000e95133
lzlzlz911 <lzlzlz911@hotmail.com>
2016-02-05 11:00:02 +0800

    y1

---
+++ Orleans4Net.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orleans4Net.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
